d03dfbb8d367d24a24d26088824db3fd74b5f331
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2025-04-16 20:00:09 +0300

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Angular Template
+
+This is a simple Angular starter template with some common configured items.
+
+### Configured items
+
+- Prettier
+- EsLint
+- Commit-lint
+- Husky(with commit lint and pretty quick)
---
+++ eslint.config.mjs
@@ -0,0 +1,60 @@
+// @ts-check
+import eslint from '@eslint/js'
+import tsEslint from 'typescript-eslint'
+import angular from 'angular-eslint'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import unusedImports from 'eslint-plugin-unused-imports'
+
+export default tsEslint.config(
+	{
+		ignores: ['eslint.config.mjs'],
+	},
+	{
+		files: ['**/*.ts'],
+		extends: [eslint.configs.recommended, ...tsEslint.configs.recommended, ...tsEslint.configs.stylistic, ...angular.configs.tsRecommended],
+		processor: angular.processInlineTemplates,
+		rules: {
+			'@angular-eslint/directive-selector': [
+				'error',
+				{
+					type: 'attribute',
+					prefix: 'app',
+					style: 'camelCase',
+				},
+			],
+			'@angular-eslint/component-selector': [
+				'error',
+				{
+					type: 'element',
+					prefix: 'app',
+					style: 'kebab-case',
+				},
+			],
+		},
+	},
+	{
+		files: ['**/*.html'],
+		extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+	},
+	{
+		plugins: {
+			'simple-import-sort': simpleImportSort,
+			'unused-imports': unusedImports,
+		},
+		rules: {
+			'simple-import-sort/imports': 'error',
+			'simple-import-sort/exports': 'error',
+			'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+			'unused-imports/no-unused-imports': 'error',
+			'unused-imports/no-unused-vars': [
+				'warn',
+				{
+					vars: 'all',
+					varsIgnorePattern: '^_',
+					args: 'after-used',
+					argsIgnorePattern: '^_',
+				},
+			],
+		},
+	},
+)
