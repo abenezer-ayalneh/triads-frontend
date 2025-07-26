7dfd1fcf6d877092c2197887a669e3ce56d690eb
Abenezer Ayalneh <abenezer.ayalneh.42@gmail.com>
2025-07-26 12:08:52 +0300

    chore: rename the app component to comply with angular 20 naming convention

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,28 +1,28 @@
 import { TestBed } from '@angular/core/testing'
 
-import { AppComponent } from './app.component'
+import { App } from './app'
 
-describe('AppComponent', () => {
+describe('App', () => {
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [AppComponent],
+			imports: [App],
 		}).compileComponents()
 	})
 
 	it('should create the app', () => {
-		const fixture = TestBed.createComponent(AppComponent)
+		const fixture = TestBed.createComponent(App)
 		const app = fixture.componentInstance
 		expect(app).toBeTruthy()
 	})
 
 	it(`should have the 'triads' title`, () => {
-		const fixture = TestBed.createComponent(AppComponent)
+		const fixture = TestBed.createComponent(App)
 		const app = fixture.componentInstance
 		expect(app.title).toEqual('triads')
 	})
 
 	it('should render title', () => {
-		const fixture = TestBed.createComponent(AppComponent)
+		const fixture = TestBed.createComponent(App)
 		fixture.detectChanges()
 		const compiled = fixture.nativeElement as HTMLElement
 		expect(compiled.querySelector('h1')?.textContent).toContain('Hello, triads')
