769c25151922e4c8316d2e8530c01d07a276e254
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2026-02-04 10:50:31 +0300

    chore(packages): update packages to latest version & add ionic config file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,7 @@ export const environment = {
 	production: true,
 	apiUrl: 'http://localhost:3000/api',
 	// apiUrl: 'http://192.168.8.123:3000/api',
+	// apiUrl: 'https://dogfish-together-locust.ngrok-free.app/api',
+	// apiUrl: 'https://triads-api.gametrix.org/api',
 	timeout: 10000,
 }
