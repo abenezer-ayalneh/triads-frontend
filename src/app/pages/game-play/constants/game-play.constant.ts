43359f18fa418c2f015abcfcf676f4115d1034a3
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2026-02-10 20:33:05 +0300

    fix(game-play): update game end buttons hover effect

--- src/app/pages/game-play/constants/game-play.constant.ts
+++ src/app/pages/game-play/constants/game-play.constant.ts
@@ -1,11 +1,11 @@
 export const WRONG_MESSAGES = ['Oops!', 'Nope', 'Incorrect', 'Missed it!', 'Try Again', 'Error']
 
 export const GAME_END_MESSAGES: Record<number, string> = {
-	15: 'WOW! You did it! A perfect score! Go again?',
+	15: 'WOW! You did it! A perfect score!',
 	12: 'Nice one! Think you can do even better?',
 	10: 'Success! (but just barely). Want another try?',
 	8: 'Close, but the last one got you. Try another?',
-	6: 'That was a tough one. Want to try again?',
+	6: 'That was a tough one. Ready to try again?',
 	3: 'You can do better, give it another try.',
 	0: "Try another? You'll get the hang of it.",
 }
